1164fb58ff1674292b31c27692d0018e39304ad3
Venkatesh0519 <52618163+Venkatesh0519@users.noreply.github.com>
2019-07-18 16:57:27 +0530

    Add files via upload

---
+++ Mad Lib Generator.py
@@ -0,0 +1,12 @@
+#Mad Lib Game in Python
+
+celebrity=input("Enter the Name: ")
+adjective=input("Enter an adjective: ")
+number=input("Enter the number: ")
+pluralnoun=input("Enter the pluralnoun: ")
+noun=input("Enter the noun: ")
+
+print("Are you okay? I asked")
+print(celebrity+ " " + "answered: I'm" +" " +adjective,"thanks!")
+print("At that point,"+number,"government agents burst in,armed with"+" "+pluralnoun,". Impressed by my"+" "+noun,", they recruited me "
+                                                                                                          "into the FBI on the spot.")
